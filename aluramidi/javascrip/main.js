508e3c8ba7d6311e90277f292d6f8a388b8bfb2b
Paola Soares <soaresgpaola@gmail.com>
2023-03-28 00:03:09 -0300

    Página Dinâmica

---
+++ aluramidi/javascrip/main.js
@@ -0,0 +1,31 @@
+//chamar a função
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName == 'audio') {
+    elemento.play();
+  }else{
+    console.log('Elemento não encontrado ou seletor não inválido')
+  }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;//template string, craze era o que faltava`´
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+  tecla.onkeydown = function (evento) {
+    //se (dada condição for verdadeira)------------------------------------>
+    if (evento.code === "Enter" || evento.code == "Space") { //<----------------
+      tecla.classList.add('ativa');
+    }
+  }
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
+  }
+}
